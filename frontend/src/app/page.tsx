a393fc7edcc3f1b775dab54ab5eb9c9a296c4ac4
Andreandhiki <andhiki28@gmail.com>
2024-10-14 23:08:04 +0700

    feat: add smooth scroll

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <main className="gap-10 flex-col">
+        <div className="w-full h-[50vh] bg-blue-500">
+
+        </div>
+        <div className="w-full h-[50vh] bg-red-500">
+
+        </div>
+        <div className="w-full h-[50vh] bg-blue-500">
+
+        </div>
+        <div className="w-full h-[50vh] bg-red-500">
+
+        </div>
+      </main>
+      <footer className="w-full h-1/2 bg-orange-500">
+        
+      </footer>
+    </div>
+  );
+}
