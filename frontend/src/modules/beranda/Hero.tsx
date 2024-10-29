ab10ec55d6bd19a3139cf89f92330e0f33cf64ac
Devin <sultandevinosuyudi@mail.ugm.ac.id>
2024-10-29 23:17:31 +0700

    feat: diddy

--- frontend/src/modules/beranda/Hero.tsx
+++ frontend/src/modules/beranda/Hero.tsx
@@ -6,6 +6,7 @@ import omahti from "@/../public/logos/omahti.svg";
 import Link from "next/link";
 import Marquee from "@/components/ui/marquee";
 import { House } from "lucide-react";
+import diddy from "@/../public/diddy.png";
 
 const Hero = () => {
   return (
@@ -30,7 +31,7 @@ const Hero = () => {
 
 const TitleCTA = () => (
   <div className="z-20 flex flex-col items-center gap-4">
-    <h1 className="text-center text-3xl font-semibold drop-shadow-2xl sm:text-5xl">
+    <h1 className="text-center text-3xl font-semibold drop-shadow-xl sm:text-5xl">
       Be Great. Be Us.
     </h1>
     <Link href={`dashboard`}>
@@ -50,11 +51,17 @@ const Logos = () => (
 
 const Background = () => (
   <>
+    {/* image */}
+    <div className="absolute inset-0">
+      <div className="relative h-full w-full">
+        <Image className="z-10 object-contain" src={diddy} sizes="50%" fill alt="Diddy" />
+      </div>
+    </div>
     {/* circle in the background */}
-    <div className="absolute bottom-0 lg:bottom-[-10rem] z-0 aspect-square h-auto w-full max-w-xl  rounded-full bg-custom-gray" />
+    <div className="absolute bottom-0 z-0 aspect-square h-auto w-full max-w-xl rounded-full bg-custom-gray lg:bottom-[-10rem]" />
     {/* gradients */}
-    <div className="absolute bottom-0 left-0 right-1/2 top-0 bg-gradient-to-t from-custom-blue via-transparent to-transparent" />
-    <div className="absolute bottom-0 left-1/2 right-0 top-0 bg-gradient-to-t from-custom-orange via-transparent to-transparent" />
+    <div className="absolute bottom-0 left-0 right-1/2 top-0 z-10 bg-gradient-to-t from-custom-blue via-transparent to-transparent" />
+    <div className="absolute bottom-0 left-1/2 right-0 top-0 z-10 bg-gradient-to-t from-custom-orange via-transparent to-transparent" />
   </>
 );
 
@@ -68,7 +75,7 @@ const BenefitsMarquee = () => {
   return (
     <Marquee className="bg-custom-gray-dark">
       {Benefits.map((benefit) => (
-        <div className="gap-2 items-center font-medium flex flex-row shrink-0 mx-4 py-2 sm:py-4 md:py-8 text-custom-silver sm:mx-8 lg:mx-16 lg:text-lg">
+        <div className="mx-4 flex shrink-0 flex-row items-center gap-2 py-2 font-medium text-custom-silver sm:mx-8 sm:py-4 md:py-8 lg:mx-16 lg:text-lg">
           <House size={24} className="h-4 lg:h-5" />
           {benefit}
         </div>
