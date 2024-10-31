b77243ae4ab930064c9000af5f2e490415c58045
shampoerna <153973086+shampoerna@users.noreply.github.com>
2024-10-31 23:31:21 +0700

    feature: pop up

--- frontend/src/modules/wawancara/components/PopupKonfirmasi.tsx
+++ frontend/src/modules/wawancara/components/PopupKonfirmasi.tsx
@@ -12,7 +12,7 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 
-export default function Pilih({ className }: { className?: string }) {
+export default function PopupKonfirmasi({ className }: { className?: string }) {
   return (
     <AlertDialog>
       <AlertDialogTrigger asChild>
