de0de5d0a7c082303a3869d2721726c0be99cb2e
shampoerna <153973086+shampoerna@users.noreply.github.com>
2024-11-01 11:35:31 +0700

    feat: semua pop up insyaallah sudah ya

--- frontend/src/modules/wawancara/components/PilihanJamCard.tsx
+++ frontend/src/modules/wawancara/components/PilihanJamCard.tsx
@@ -1,9 +1,10 @@
 "use client";
 import React from "react";
-import Pilih from "@/modules/wawancara/components/PopupKonfirmasi"
 import JadwalWawancara from "./JadwalWawancara";
 import Selesai from "./PopupJadwalBerhasil";
 import Gagal from "./PopupGagal";
+import PopupKonfirmasi from "@/modules/wawancara/components/PopupKonfirmasi";
+import PopupGagal from "./PopupGagal";
 
 
 interface PilihanWaktuProps {
@@ -25,14 +26,10 @@ const PilihanWaktuCard = ({ variant = "omahti", onSelect }: PilihanWaktuProps) =
 
   return (
     <div className="flex flex-col items-center bg-custom-gray-dark rounded-lg p-4 w-full"> 
-      {/* Header */}
       <div className="flex justify-between items-center w-full mb-4">
-        {/* Dynamic Title */}
         {foo}
-        <Pilih />
+        <PopupGagal />
       </div>
-
-      {/* Content Area */}
       <JadwalWawancara category={variant === "himakom" ? "Himakom" : "OmahTI"} />
     </div>
   );
