8cd5583634b06a5c08a61cb01cf809d0ad2feab2
vityasyyy <argyavityasy1208@gmail.com>
2024-11-03 19:00:13 +0700

    feat: fetching pilihan wawancara sudah

--- frontend/src/modules/wawancara/wawancara.tsx
+++ frontend/src/modules/wawancara/wawancara.tsx
@@ -3,13 +3,23 @@ import Peringatan from "./components/peringatan";
 import WaktuPilihan from "./components/WaktuPilihan";
 import PilihanWaktu from "./components/PilihanJam";
 
-export default function Divisi() {
+import {getPilihanWawancara, getAllWawancara} from "@/utils/fetch";
+import {getCurrentUser} from "@/utils/auth";
+import { cookies } from "next/headers";
+export default async function Divisi() {
+  const accessToken = cookies().get("accessToken")?.value;
+  const user = await getCurrentUser();
+  const enrolledSlugHima = user?.enrolledSlugHima;
+  const enrolledSlugOti = user?.enrolledSlugOti;
+  const wawancara = await getAllWawancara();
+  const {filteredHima, filteredOti} = await getPilihanWawancara(accessToken as string);
+  console.log(filteredHima, filteredOti);
   return (
     <>
       <Title />
       <hr className={`my-4 border-2 border-custom-gray-dark`} />
       <Peringatan />
-      <WaktuPilihan />
+      <WaktuPilihan filteredHima={filteredHima} slugOti={enrolledSlugOti} slugHima={enrolledSlugHima} filteredOti={filteredOti}/>
       <PilihanWaktu />
     </>
   );
