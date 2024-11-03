196205e7290cf18b5fd603c26497bc7db2d6c4b7
vityasyyy <argyavityasy1208@gmail.com>
2024-11-03 19:00:13 +0700

    feat: fetching pilihan wawancara sudah

--- frontend/src/modules/wawancara/components/WaktuPilihan.tsx
+++ frontend/src/modules/wawancara/components/WaktuPilihan.tsx
@@ -1,22 +1,34 @@
 import Card from "./WaktuPilihanCard";
 
-const WaktuPilihan = () => {
+const WaktuPilihan = ({ filteredHima, filteredOti, slugOti, slugHima }: any) => {
+  const isEmpty = !filteredHima && !filteredOti;
+
   return (
     <div className="my-8">
       <h5 className="font-semibold lg:text-lg">Waktu Wawancara yang Kamu Pilih</h5>
       <div className="flex flex-col gap-2 mt-2 xl:flex-row">
-        <Card
-          variant="omahti"
-          tanggal="27 November 2024"
-          jam="19:00"
-          lokasi="IUP Room"
-        />
-        <Card
-          variant="himakom"
-          tanggal="27 November 2024"
-          jam="19:00"
-          lokasi="IUP Room"
-        />
+        {isEmpty ? (
+          <p>Kamu belum memilih wawancara</p>
+        ) : (
+          <>
+            {filteredOti && (
+              <Card
+                variant="omahti"
+                tanggal={filteredOti.tanggal}
+                jam={filteredOti.sesi[0].jam}
+                lokasi={filteredOti.sesi[0].slotDivisi[slugOti]?.lokasi}
+              />
+            )}
+            {filteredHima && (
+              <Card
+                variant="himakom"
+                tanggal={filteredHima.tanggal}
+                jam={filteredHima.sesi[0].jam}
+                lokasi={filteredHima.sesi[0].slotDivisi[slugHima]?.lokasi}
+              />
+            )}
+          </>
+        )}
       </div>
     </div>
   );
