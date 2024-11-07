af4048e27d04c725da27cfcdac5735889c84c448
vityasyyy <argyavityasy1208@gmail.com>
2024-11-07 19:06:00 +0700

    config: next config update

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -10,6 +10,27 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*', // Match all paths
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://oprec.makomti.web.id', // You can restrict this to specific origins like 'https://yourdomain.com'
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS', // Allow only specific methods
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization', // Allow specific headers
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
