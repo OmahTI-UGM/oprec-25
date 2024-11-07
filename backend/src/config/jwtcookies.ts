03e380603014e10ebb6f490cf2c6728c4a7425cf
vityasyyy <argyavityasy1208@gmail.com>
2024-11-07 16:40:00 +0700

    feat: added new cookie configuration

--- backend/src/config/jwtcookies.ts
+++ backend/src/config/jwtcookies.ts
@@ -11,5 +11,6 @@ export const COOKIE_CONFIG: CookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
     sameSite: 'strict',
-    maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    domain: process.env.COOKIE_DOMAIN || ""
 };
