500450fde22fea79de0ae48c2ea81f24575fc345
Devin <sultandevinosuyudi@mail.ugm.ac.id>
2024-11-10 02:17:54 +0700

    feat: added loading ui in dashboard

---
+++ frontend/app/(dashboard)/loading.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import LoadingGif from "@/../public/assets/components/Loading.gif";
+
+const Loading = () => {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <Image src={LoadingGif} className="h-auto w-48" alt="Loading GIF" />
+    </div>
+  );
+};
+
+export default Loading;
