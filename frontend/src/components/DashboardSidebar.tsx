cdaf6ffc4862b142d5d6e6491655960896a78b1f
vityasyyy <argyavityasy1208@gmail.com>
2024-11-10 14:37:15 +0700

    fix: nomor telepon nasya

--- frontend/src/components/DashboardSidebar.tsx
+++ frontend/src/components/DashboardSidebar.tsx
@@ -39,7 +39,7 @@ const DashboardSidebar = ({ admin = false }: { admin?: boolean }) => {
         // },
         {
           icon: <CircleHelp className="h-5 shrink-0" />,
-          href: "https://wa.me/6287778480114",
+          href: "https://wa.me/6289603375791",
           tag: "Bantuan",
           newTab: true,
         },
