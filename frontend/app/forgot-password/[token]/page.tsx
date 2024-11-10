956f22b95ac4211ed0fbb5f2b6717cc2c587431a
Andreandhiki <andhiki28@gmail.com>
2024-11-10 13:44:01 +0700

    fix[forgot-password]: add ui to forgot password

--- frontend/app/forgot-password/[token]/page.tsx
+++ frontend/app/forgot-password/[token]/page.tsx
@@ -2,6 +2,18 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import Container from "@/components/Container";
+import MiniFooter from "@/components/MiniFooter";
+import Image from "next/image";
+import himakom from "@/logos/himakom.svg";
+import omahti from "@/logos/omahti.svg";
+import { Eye, EyeOff, Info } from "lucide-react";
+import { useForm } from "react-hook-form";
+
+interface FormData {
+  newPassword: string;
+  confirmPassword: string;
+}
 
 type PassresetPageProps = {
   params: {
@@ -10,17 +22,32 @@ type PassresetPageProps = {
 };
 
 const ResetPasswordPage = ({ params }: PassresetPageProps) => {
-  const { token } = params; // Get the token from the URL
+  const { token } = params;
   const router = useRouter();
-  const [newPassword, setNewPassword] = useState("");
   const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
+  const [hidePassword, setHidePassword] = useState(true);
+  const [hideConfirmPassword, setHideConfirmPassword] = useState(true);
+  
+  const { register, handleSubmit, formState: { errors }, watch } = useForm<FormData>();
+  const newPassword = watch("newPassword");
+  const confirmPassword = watch("confirmPassword");
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault(); // Prevent default form submission
+  const passwordValidation = {
+    required: "Password is required",
+    minLength: {
+      value: 8,
+      message: "Password must be at least 8 characters"
+    },
+    pattern: {
+      value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*()_+[\]{}|\\;',./:<>?`~"-]{8,}$/,
+      message: "Password must have at least 8 characters and contain one number."
+    }
+  };
 
-    if (!newPassword) {
-      setError("Password is required");
+  const onSubmit = async (data: FormData) => {
+    if (data.newPassword !== data.confirmPassword) {
+      setError("Passwords do not match");
       return;
     }
 
@@ -30,19 +57,19 @@ const ResetPasswordPage = ({ params }: PassresetPageProps) => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ token, newPassword }),
+        body: JSON.stringify({ token, newPassword: data.newPassword }),
       });
 
-      const data = await response.json();
+      const responseData = await response.json();
 
       if (response.ok) {
         setSuccess("Password reset successfully! Redirecting to login page...");
         setError("");
         setTimeout(() => {
-          router.push("/auth/login"); // Redirect to login after success
+          router.push("/auth/login");
         }, 2000);
       } else {
-        setError(data.error || "An error occurred");
+        setError(responseData.error || "An error occurred");
         setSuccess("");
       }
     } catch (err) {
@@ -52,39 +79,93 @@ const ResetPasswordPage = ({ params }: PassresetPageProps) => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen space-y-8">
-      <h1 className="text-3xl font-semibold text-center text-custom-blue-dark sm:text-4xl">
-        Reset Your Password
-      </h1>
+    <>
+      <section>
+        <Container
+          className="flex flex-col items-center justify-center bg-custom-black"
+          parentClass="min-h-screen"
+        >
+          <div className="absolute inset-0 overflow-hidden">
+            <div className="absolute right-[-5%] top-[-10%] h-[800px] w-[800px] rounded-full bg-custom-blue/10 blur-[120px]" />
+            <div className="absolute bottom-[19%] left-[-10%] h-[600px] w-[600px] rounded-full bg-custom-orange/10 blur-[100px]" />
+          </div>
+          <div className="relative gap-4 bg-custom-black rounded-lg border border-custom-silver/15 p-4 pt-10 flex w-full max-w-sm flex-col items-center">
+            <Logos />
+            <h1 className="text-center text-3xl font-semibold text-white">
+              Reset Password
+            </h1>
+            <form onSubmit={handleSubmit(onSubmit)} className="flex w-full max-w-sm rounded-lg flex-col">
+              <div className="space-y-2 mb-4">
+                <label className={`${errors.newPassword && "text-red-500"}`}>
+                  New Password
+                </label>
+                <div className="relative">
+                  <input
+                    type={hidePassword ? "password" : "text"}
+                    {...register("newPassword", passwordValidation)}
+                    className={`w-full rounded-lg border border-white/10 bg-black/10 px-4 py-2 text-white placeholder-custom-gray-light focus:border-custom-blue focus:outline-none focus:ring-1 focus:ring-custom-blue ${errors.newPassword && "border-red-500"}`}
+                  />
+                  <span className="absolute right-1 top-1/2 -translate-y-1/2">
+                    <div className="cursor-pointer p-2 text-custom-silver" onClick={() => setHidePassword(!hidePassword)}>
+                      {hidePassword ? <EyeOff size={18} /> : <Eye size={18} />}
+                    </div>
+                  </span>
+                </div>
+                {errors.newPassword && (
+                  <p className="flex gap-1.5 text-sm text-red-500">
+                    <Info size={10} className="mt-1 shrink-0" /> {errors.newPassword.message}
+                  </p>
+                )}
+              </div>
 
-      <form
-        onSubmit={handleSubmit}
-        className="w-full max-w-md p-6 bg-white rounded-lg shadow-md border-[1px] border-custom-blue-dark"
-      >
-        <h2 className="mb-6 text-2xl font-bold text-center text-custom-blue-dark">
-          Enter New Password
-        </h2>
+              <div className="space-y-2 mb-4">
+                <label className={`${errors.confirmPassword && "text-red-500"}`}>
+                  Confirm Password
+                </label>
+                <div className="relative">
+                  <input
+                    type={hideConfirmPassword ? "password" : "text"}
+                    {...register("confirmPassword", { 
+                      required: "Confirm password is required",
+                      validate: (value) => value === newPassword || "Passwords do not match"
+                    })}
+                    className={`w-full rounded-lg border border-white/10 bg-black/10 px-4 py-2 text-white placeholder-custom-gray-light focus:border-custom-blue focus:outline-none focus:ring-1 focus:ring-custom-blue ${errors.confirmPassword && "border-red-500"}`}
+                  />
+                  <span className="absolute right-1 top-1/2 -translate-y-1/2">
+                    <div className="cursor-pointer p-2 text-custom-silver" onClick={() => setHideConfirmPassword(!hideConfirmPassword)}>
+                      {hideConfirmPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+                    </div>
+                  </span>
+                </div>
+                {errors.confirmPassword && (
+                  <p className="flex gap-1.5 text-sm text-red-500">
+                    <Info size={10} className="mt-1 shrink-0" /> {errors.confirmPassword.message}
+                  </p>
+                )}
+              </div>
 
-        <input
-          type="password"
-          placeholder="New Password"
-          value={newPassword}
-          onChange={(e) => setNewPassword(e.target.value)}
-          required
-          className="w-full px-4 py-2 mb-4 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
+              {error && <p className="mb-4 text-sm text-red-500">{error}</p>}
+              {success && <p className="mb-4 text-sm text-green-500">{success}</p>}
 
-        {error && <p className="mb-4 text-sm text-red-500">{error}</p>}
-        {success && <p className="mb-4 text-sm text-green-500">{success}</p>}
-
-        <div className="flex justify-center">
-          <Button variant="secondary" type="submit">
-            Reset Password
-          </Button>
-        </div>
-      </form>
-    </div>
+              <div className="flex justify-center mt-6">
+                <Button variant="secondary" type="submit" className="w-full">
+                  Change Password
+                </Button>
+              </div>
+            </form>
+          </div>
+        </Container>
+        <MiniFooter />
+      </section>
+    </>
   );
 };
 
 export default ResetPasswordPage;
+
+const Logos = () => (
+  <div className="z-20 flex items-center gap-2">
+    <Image src={himakom} alt="Logo Himakom" className="h-8 w-auto" />
+    <Image src={omahti} alt="Logo OmahTI" className="h-8 w-auto" />
+  </div>
+);
