3743d85999eb475dff4c42f7c06c83dacc872855
Devin <sultandevinosuyudi@mail.ugm.ac.id>
2024-11-10 13:23:39 +0700

    feat: enhance admin dashboard UI and improve user approval flow

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -20,7 +20,7 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-custom-gray/20 dark:hover:bg-neutral-800 dark:hover:text-neutral-50",
         link: "text-neutral-900 underline-offset-4 hover:underline dark:text-neutral-50",
-        white: "bg-custom-silver text-custom-black shadow-sm hover:bg-custom-silver/90 dark:bg-custom-silver dark:text-custom-silver dark:hover:bg-custom-black/90",
+        white: "bg-custom-silver text-custom-black shadow-sm hover:bg-custom-silver/80 dark:bg-custom-silver dark:text-custom-silver dark:hover:bg-custom-black/90",
         whiteOutline: "bg-transparent border border-custom-silver text-custom-silver shadow-sm hover:bg-custom-silver/10 dark:border-neutral-800 dark:bg-neutral-950 dark:hover:bg-neutral-800",
       },
       size: {
--- frontend/src/modules/admin/adminDashboard.tsx
+++ frontend/src/modules/admin/adminDashboard.tsx
@@ -1,107 +1,143 @@
-'use client'
-import { ChevronLeft } from 'lucide-react';
-import Image from 'next/image';
-import { Logos } from '@/utils/types';
+"use client";
+import Image from "next/image";
+import { Logos } from "@/utils/types";
+import { Button } from "@/components/ui/button";
+import { EyeIcon, LoaderCircle, UserRoundCheck } from "lucide-react";
+import Link from "next/link";
+import { useToast } from "@/hooks/use-toast";
+import { useState } from "react";
 
-const AdminDashboard = ({allUsers, admin}: {allUsers: any, admin: any}) => {
+const AdminDashboard = ({ allUsers, admin }: { allUsers: any; admin: any }) => {
+  const [pending, setPending] = useState(false);
+  const { toast } = useToast();
+
   const handleApprove = async (userId: string, acceptDivisionId: string) => {
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/adminonly/admin/`, {
-        method: "POST",
-        credentials: 'include',
-        headers: {
-          "Content-Type": "application/json"
+      setPending(true);
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/adminonly/admin/`,
+        {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ userId, acceptDivisionId }),
         },
-        body: JSON.stringify({userId, acceptDivisionId})
+      );
+
+      toast({
+        title: "User approved",
+        description: "User has been approved and accepted to the division.",
       });
+
       if (!res.ok) {
+        toast({
+          variant: "destructive",
+          title: "Failed to approve user",
+          description: "Sorry, something went wrong. Please try again later.",
+        });
         throw new Error("Failed to approve user");
       }
     } catch (err) {
+      toast({
+        variant: "destructive",
+        title: "Failed to approve user",
+        description: "Sorry, something went wrong. Please try again later.",
+      });
       console.error(err);
+    } finally {
+      setPending(false);
     }
-  }
+  };
 
   return (
-    <div className="min-h-screen bg-custom-black text-white">
+    <div className="min-h-screen bg-custom-black text-custom-silver">
       {/* Header */}
-      <div className="mb-8">
-        <button className="flex items-center text-gray-300 hover:text-white mb-6">
-          <ChevronLeft className="w-5 h-5 mr-1" />
-          Kembali
-        </button>
-        
-        <div className="flex items-start gap-6 justify-center h-[8rem]">
-          <Image src={Logos[admin.username as keyof typeof Logos]} alt="" width={0} height={0} className="h-full w-auto"/>
-          <div className="flex-1 justify-center items-center h-full">
-            <h1 className="text-5xl font-semibold h-full flex items-center">{(admin.username as string).toUpperCase()}</h1>
-          </div>
+      <div className="mb-4">
+        <div className="flex items-center gap-4">
+          <Image
+            src={Logos[admin.username as keyof typeof Logos]}
+            alt=""
+            width={0}
+            height={0}
+            className="w-[2rem] sm:w-[3rem]"
+          />
+          <h1 className="flex h-full items-center text-[2rem] font-semibold sm:text-[3rem]">
+            {admin.username.toUpperCase()}
+          </h1>
         </div>
       </div>
 
       {/* Table */}
-      <div className="bg-custom-gray-dark rounded-lg overflow-hidden">
+      <div className="overflow-x-auto rounded-lg bg-custom-gray-dark">
         <div className="px-6 py-4 text-lg font-medium">Informasi Pendaftar</div>
-        <table className="w-full text-center">
+        <table className="w-full">
           <thead>
-            <tr className="bg-custom-gray">
-              <th className="px-6 py-3 text-sm font-medium">No</th>
-              <th className="px-6 py-3 text-sm font-medium">Name</th>
-              <th className="px-6 py-3 text-sm font-medium">Status</th>
-              <th className="px-6 py-3 text-sm font-medium">Penugasan</th>
-              <th className="px-6 py-3 text-sm font-medium">Diterima Di</th>
+            <tr className="bg-custom-gray *:px-6 *:py-3 *:text-start *:text-[0.9rem] *:font-semibold">
+              <th>No</th>
+              <th>Name</th>
+              <th>Penugasan</th>
+              <th>Status Penerimaan</th>
             </tr>
           </thead>
           <tbody>
             {allUsers && allUsers.length > 0 ? (
               allUsers.map((user: any, index: number) => (
-                <tr key={user._id} className="border-t border-gray-700">
-                  <td className="px-6 py-4">{index + 1}</td>
-                  <td className="px-6 py-4 text-justify">{user.username}</td>
-                  <td className="px-6 py-4">
-                    {(user.tugas.length > 0) ? (
-                      <span className="text-green-400">Sudah Mengumpulkan</span>
+                <tr
+                  key={user._id}
+                  className="border-t border-gray-700 *:px-6 *:py-4 *:text-sm"
+                >
+                  <td>{index + 1}</td>
+                  <td>{user.username}</td>
+                  <td className="">
+                    {user.tugas.length > 0 ? (
+                      <Link
+                        href={user.tugas[0].link}
+                        target="_blank"
+                        rel="noreferrer noopener"
+                      >
+                        <Button size={`lg`} variant={`white`}>
+                          <EyeIcon size={16} />
+                          Lihat
+                        </Button>
+                      </Link>
                     ) : (
-                      <span className="text-red-500">Belum Mengumpulkan</span>
+                      <p className="text-sm text-custom-red opacity-80">
+                        Belum ada penugasan
+                      </p>
                     )}
                   </td>
-                  <td className="px-6 py-4">
-                  {
-                    (user.tugas.length > 0) ? (
-                      <a 
-                      className="px-4 py-2 bg-black hover:bg-gray-600 rounded-lg flex items-center justify-center gap-2 text-sm"
-                      href={user.tugas[0].link}
-                    >
-                      <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
-                      </svg>
-                      Unduh
-                    </a>
-                    ) : (
-                      <button 
-                      className="py-2 text-custom-gray-light rounded-lg text-sm"
-                      disabled
-                    >
-                      Belum ada penugasan
-                    </button>
-                    )
-                  }
-                  </td>
-                  <td className="px-6 py-4">
-                  {!user.diterimaDi ? (
-                      <button 
-                        className="px-4 py-2 bg-yellow-400 hover:bg-white hover:text-black border-custom-black text-custom-black rounded-lg gap-2 text-sm"
-                        onClick={() => handleApprove(user._id, user.adminDivision._id)}
+                  <td className="">
+                    {!user.diterimaDi ? (
+                      <Button
+                        size={`lg`}
+                        className=""
+                        variant={`whiteOutline`}
+                        onClick={() =>
+                          handleApprove(user._id, user.adminDivision._id)
+                        }
                       >
-                        Pencet untuk approve user ke divisimu
-                      </button>
+                        {pending ? (
+                          <>
+                            <LoaderCircle size={16} className="animate-spin" />
+                            Approving...
+                          </>
+                        ) : (
+                          <>
+                            <UserRoundCheck size={16} />
+                            Terima ke divisi
+                          </>
+                        )}
+                      </Button>
                     ) : (
-                      <button 
-                        className="py-2 text-custom-white rounded-lg text-sm"
-                        disabled
-                      >
-                        User sudah diterima di <span className='text-red-500'>{user.diterimaDi.judul}</span>
-                      </button>
+                      <p className="text-sm">
+                        User sudah diterima di{" "}
+                        <span className="text-custom-orange">
+                          {" "}
+                          {user.diterimaDi.judul}
+                        </span>
+                      </p>
                     )}
                   </td>
                 </tr>
@@ -109,7 +145,7 @@ const AdminDashboard = ({allUsers, admin}: {allUsers: any, admin: any}) => {
             ) : (
               <tr>
                 <td colSpan={4} className="px-6 py-4 text-center text-gray-500">
-                  No users found.
+                  Belum ada pendaftar.
                 </td>
               </tr>
             )}
